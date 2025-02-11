1421fa1e47523fd71b28f85e989485eeab5346d5
Michael Bell <mmbell@colostate.edu>
2025-02-11 16:41:22 -0700

    Fix another tileTransform bug in R grid

--- src/r_grid.jl
+++ src/r_grid.jl
@@ -180,9 +180,9 @@ function gridTransform!(patchSplines::Array{Spline1D}, patchSpectral::Array{Floa
         SAtransform!(patchSplines[i])
 
         # Assign to the tile grid
-        SItransform(patchSplines[i],getGridpoints(tile),view(tile.physical,:,1,1))
-        SIxtransform(patchSplines[i],getGridpoints(tile),view(tile.physical,:,1,2))
-        SIxxtransform(patchSplines[i],getGridpoints(tile),view(tile.physical,:,1,3))
+        SItransform(patchSplines[i],getGridpoints(tile),view(tile.physical,:,i,1))
+        SIxtransform(patchSplines[i],getGridpoints(tile),view(tile.physical,:,i,2))
+        SIxxtransform(patchSplines[i],getGridpoints(tile),view(tile.physical,:,i,3))
     end
 
     return tile.physical
